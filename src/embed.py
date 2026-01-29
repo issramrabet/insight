7c8c4f525aafc24083833f63571843887f11729a
issramrabet <issramrabet@gmail.com>
2026-01-29 17:55:38 +0100

    modified

--- src/embed.py
+++ src/embed.py
@@ -3,7 +3,7 @@ from sentence_transformers import SentenceTransformer as st
 import numpy as np
 from qdrant_client import QdrantClient as qc
 from qdrant_client.models import VectorParams, Distance, PointStruct
-f=open("data/products.json","r")
+f=open("../data/products.json","r")
 products=json.load(f)
 f.close()
 model=st("all-MiniLM-L6-v2")
@@ -35,6 +35,17 @@ client.upsert(
     collection_name="products",
     points=pts
 )
+command=input("Enter search command: ")
+qv=model.encode(command,normalize_embeddings=True).tolist()
+results=client.query_points(
+    collection_name="products",
+    query=qv,
+    limit=6
+).points
+for r in results:
+    print(r.payload["name"],r.score)
+    
+    
 
 
 
